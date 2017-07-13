cc7bedc584b7542f5b01a935ee1ef7a90de399db
Autumn <autumncbullard@gmail.com>
2017-07-13 19:17:15 -0400

    Added JS functionality

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$.getJSON('../quotes.json', function(data) {
+
+  function newQuote() {
+    var randomQuote = data[Math.floor(Math.random() * data.length)];
+    $('q').html(randomQuote.quote);
+    $('#source').html('&mdash; ' + randomQuote.source);
+    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + '\"' + randomQuote.quote + '\"' + ' — ' + randomQuote.source);
+  }
+
+  newQuote();
+
+  $('#new-quote').click(function() {
+    newQuote();
+  });
+});
