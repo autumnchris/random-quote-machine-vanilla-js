937eaa46e8859e4c4f2ca5ffdad1181c7a0dad77
Autumn <autumncbullard@gmail.com>
2017-09-26 16:37:52 -0400

    Indent the strings of HTML tags in the quoteDiv variable

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ $.ajax({
 
   function newQuote() {
     var randomQuote = data[Math.floor(Math.random() * data.length)];
-    var quoteDiv = '<span class="fa fa-quote-left"></span><q>&nbsp;' + randomQuote.quote + '&nbsp;</q><span class="fa fa-quote-right"></span><div id="source">&mdash; ' + randomQuote.source + '</div><div class="text-right"><a class="btn" href="" target="_blank" id="tweet"><span class="fa fa-twitter fa-lg"></span>&nbsp;Tweet</a></div>';
+    var quoteDiv = '<span class="fa fa-quote-left"></span>' +
+    '<q>&nbsp;' + randomQuote.quote + '&nbsp;</q>' +
+    '<span class="fa fa-quote-right"></span>' +
+    '<div id="source">&mdash; ' + randomQuote.source + '</div>' +
+    '<div class="text-right">' +
+      '<a class="btn" href="" target="_blank" id="tweet"><span class="fa fa-twitter fa-lg"></span>&nbsp;Tweet</a>' +
+    '</div>';
     $('.well').html(quoteDiv);
     $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + '\"' + randomQuote.quote + '\"' + ' — ' + randomQuote.source);
   }
@@ -16,5 +22,5 @@ $.ajax({
     newQuote();
   });
 }).fail(function() {
-  $('.well').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load a new quote</div>');
+  $('.well').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load a new quote.</div>');
 });
