b3e855f1aae3c02640ce917011ab46086b6c6491
Autumn <autumncbullard@gmail.com>
2017-12-29 20:55:26 -0500

    Update syntax

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ $.ajax({
   function newQuote() {
     var randomQuote = data[Math.floor(Math.random() * data.length)];
     var quoteDiv = '<span class="fa fa-quote-left"></span>' +
-    '<q>&nbsp;' + randomQuote.quote + '&nbsp;</q>' +
+    '<q> ' + randomQuote.quote + ' </q>' +
     '<span class="fa fa-quote-right"></span>' +
     '<div id="source">&mdash; ' + randomQuote.source + '</div>' +
     '<div class="text-right">' +
-      '<a class="btn" href="" target="_blank" id="tweet"><span class="fa fa-twitter fa-lg"></span>&nbsp;Tweet</a>' +
+      '<a class="btn" href="" target="_blank" id="tweet"><span class="fa fa-twitter fa-lg"></span> Tweet</a>' +
     '</div>';
     $('.well').html(quoteDiv);
     $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + '\"' + randomQuote.quote + '\"' + ' — ' + randomQuote.source);
@@ -22,5 +22,5 @@ $.ajax({
     newQuote();
   });
 }).fail(function() {
-  $('.well').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load a new quote.</div>');
+  $('.well').html('<div class="alert alert-warning"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load a new quote.</div>');
 });
