083cf243f13a794eefafc91cd32b2c80ec5d75bf
Autumn <autumncbullard@gmail.com>
2018-04-19 18:29:04 -0400

    Switch to ES6 syntax

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 $.ajax({
   dataType: 'json',
   url: 'db/quotes.min.json'
-}).done(function(data) {
+}).done(data => {
 
   function getQuote() {
-    var randomQuote = data[Math.floor(Math.random() * data.length)];
-    var quoteDiv = '<span class="fa fa-quote-left"></span>' +
-    '<q> ' + randomQuote.quote + ' </q>' +
-    '<span class="fa fa-quote-right"></span>' +
-    '<div class="source">&mdash; ' + randomQuote.source + '</div>' +
-    '<div class="text-right">' +
-      '<a class="btn tweet" href="" target="_blank"><span class="fa fa-twitter fa-lg"></span> Tweet</a>' +
-    '</div>';
+    const randomQuote = data[Math.floor(Math.random() * data.length)];
+    const quoteDiv = `<span class="fa fa-quote-left"></span>
+    <q> ${randomQuote.quote} </q>
+    <span class="fa fa-quote-right"></span>
+    <div class="source">&mdash; ${randomQuote.source}</div>
+    <div class="text-right">
+      <a class="btn tweet" href="" target="_blank"><span class="fa fa-twitter fa-lg"></span> Tweet</a>
+    </div>`;
     $('.well').html(quoteDiv);
-    $('.tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + '\"' + randomQuote.quote + '\"' + ' — ' + randomQuote.source);
+    $('.tweet').attr('href', `https://twitter.com/intent/tweet?text="${randomQuote.quote}" — ${randomQuote.source}`);
   }
 
   getQuote();
 
-  $('.new-quote').click(function() {
+  $('.new-quote').click(() => {
     getQuote();
   });
-}).fail(function() {
+}).fail(() => {
   $('.container-fluid').html('<div class="alert alert-warning"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load a new quote.</div>');
 });
