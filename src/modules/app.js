029497bde2cd36ee2770a90a36ea4fff84ca841c
autumnchris <autumncbullard@gmail.com>
2021-11-04 15:43:22 -0400

    Update portfolio link

--- src/modules/app.js
+++ src/modules/app.js
@@ -9,12 +9,12 @@ const App = (() => {
       <h1>Inspirational Quotes</h1>
     </header>
     <main></main>
-    <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; ${new Date().getFullYear()}</footer>`;
+    <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_blank">Autumn Bullard</a> &copy; ${new Date().getFullYear()}</footer>`;
 
     QuoteContainer.renderLoadingSpinner();
     NewQuote.fetchQuotes();
 
-    document.addEventListener('click', () => {
+    document.addEventListener('click', event => {
        const element = event.target;
        element.matches('.new-quote') ? NewQuote.getQuote() : null;
     });
